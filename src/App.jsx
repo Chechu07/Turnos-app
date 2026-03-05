28644e72fd6d51d4fddad0cb81feb246719e7b9d
Chechu07 <ceci.m.digital@gmail.com>
2026-03-05 10:25:44 -0300

    Corrigiendo mayúscula para el deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import Client from "./pages/Client"
 import Admin from "./pages/Admin"
-import Login from "./pages/login"
+import Login from "./pages/Login"
 
 export default function App() {
   return (
